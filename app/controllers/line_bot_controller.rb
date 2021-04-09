b8150037ee2cc2395ed4acfb5cd0d6d3d313f833
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-04-09 09:13:42 +0900

    Finished app

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -46,21 +46,168 @@ class LineBotController < ApplicationController
       'applicationId' => ENV['RAKUTEN_APPID'],
       'hits' => 5,
       'responseType' => 'small',
+      # 緯度経度タイプを世界測地系に設定
+      'datumType' => 1,
       'formatVersion' => 2
     }
     response = http_client.get(url, query)
     # 文字列からハッシュに変換
     response = JSON.parse(response.body)
-    text = ''
-    response['hotels'].each do |hotel|
-      text <<
-        hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
-          hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
-          "\n"
+    if response.key?('error')
+      text = "この検索条件に該当する宿泊施設が見つかりませんでした。\n条件を変えて再検索してください。"
+      {
+        type: 'text',
+        text: text
+      }
+    else
+      text = ''
+      response['hotels'].each do |hotel|
+        text <<
+          hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+            hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
+            "\n"
+      end
+      # flexMessageオブジェクト
+      {
+        type: 'flex',
+        altText: '宿泊検索の結果です。',
+        contents: set_carousel(response['hotels'])
+      }
     end
-    message = {
-      type: 'text',
-      text: text
+    # text = ''
+    # response['hotels'].each do |hotel|
+    #   text <<
+    #     hotel[0]['hotelBasicInfo']['hotelName'] + "\n" +
+    #       hotel[0]['hotelBasicInfo']['hotelInformationUrl'] + "\n" +
+    #       "\n"
+    # end
+    # message = {
+    #   type: 'text',
+    #   text: text
+    # }
+  end
+  def set_carousel(hotels)
+    bubbles = []
+    hotels.each do |hotel|
+      bubbles << set_bubble(hotel[0]['hotelBasicInfo'])
+    end
+    {
+      type: 'carousel',
+      contents: bubbles
+    }
+  end
+  def set_hero(hotel)
+    {
+      type: 'image',
+      url: hotel['hotelImageUrl'],
+      size: 'full',
+      aspectRatio: '20:13',
+      aspectMode: 'cover',
+      action: {
+        type: 'uri',
+        uri:  hotel['hotelInformationUrl']
+      }
+    }
+  end
+  def set_body(hotel)
+    {
+      type: 'box',
+      layout: 'vertical',
+      contents: [
+        {
+          type: 'text',
+          text: hotel['hotelName'],
+          wrap: true,
+          weight: 'bold',
+          size: 'md'
+        },
+        {
+          type: 'box',
+          layout: 'vertical',
+          margin: 'lg',
+          spacing: 'sm',
+          contents: [
+            {
+              type: 'box',
+              layout: 'baseline',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'text',
+                  text: '住所',
+                  color: '#aaaaaa',
+                  size: 'sm',
+                  flex: 1
+                },
+                {
+                  type: 'text',
+                  text: hotel['address1'] + hotel['address2'],
+                  wrap: true,
+                  color: '#666666',
+                  size: 'sm',
+                  flex: 5
+                }
+              ]
+            },
+            {
+              type: 'box',
+              layout: 'baseline',
+              spacing: 'sm',
+              contents: [
+                {
+                  type: 'text',
+                  text: '料金',
+                  color: '#aaaaaa',
+                  size: 'sm',
+                  flex: 1
+                },
+                {
+                  type: 'text',
+                  text: '￥' + hotel['hotelMinCharge'].to_s(:delimited) + '〜',
+                  wrap: true,
+                  color: '#666666',
+                  size: 'sm',
+                  flex: 5
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  end
+  def set_footer(hotel)
+    {
+      type: 'box',
+      layout: 'vertical',
+      spacing: 'sm',
+      contents: [
+        {
+          type: 'button',
+          style: 'link',
+          height: 'sm',
+          action: {
+            type: 'uri',
+            label: '電話する',
+            uri: 'tel:' + hotel['telephoneNo']
+          }
+        },
+        {
+          type: 'button',
+          style: 'link',
+          height: 'sm',
+          action: {
+            type: 'uri',
+            label: '地図を見る',
+            uri: 'https://www.google.com/maps?q=' + hotel['latitude'].to_s + ',' + hotel['longitude'].to_s
+          }
+        },
+        {
+          type: 'spacer',
+          size: 'sm'
+        }
+      ],
+      flex: 0
     }
   end
 end
